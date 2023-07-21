006f8cfd7a3d38cd9d6536418b19f202bac6b67b
Sebastian Martinez <sebastiansilvan97@gmail.com>
2023-07-21 11:39:31 -0500

    feat: back icon added

--- src/components/FoodList/index.tsx
+++ src/components/FoodList/index.tsx
@@ -1,10 +1,14 @@
-import { IonBackButton, IonButtons, IonToolbar, IonTitle } from "@ionic/react";
+import { IonBackButton, IonButtons, IonToolbar, IonTitle, IonButton, IonIcon } from "@ionic/react";
 import './foodList.css'
+import { HiChevronLeft } from "react-icons/hi2";
 import { useParams } from "react-router";
 import { UseFecthPost } from "../../api/post";
 import { PoroductoPorCategoria, ProductoCategorias } from "../../interfaces";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useHistory } from 'react-router-dom';
+
 import 'swiper/css';
+import { h } from "ionicons/dist/types/stencil-public-runtime";
 
 
 export const ListFood = () => {
@@ -19,12 +23,22 @@ export const ListFood = () => {
     console.log(data);
     const producto: PoroductoPorCategoria[] = data
 
+
+    const history = useHistory();
+
+
+    const handleBackClick = () => {
+        history.goBack();
+    };
+
     return (
         <>
             <IonToolbar>
-                <IonTitle>Hello ListFood</IonTitle>
                 <IonButtons slot="start">
-                    <IonBackButton defaultHref="//home/fitbar" />
+                    <IonButton onClick={() => handleBackClick()}>
+                        <HiChevronLeft style={{ fontSize: '2rem' }}/>
+
+                    </IonButton>
                 </IonButtons>
             </IonToolbar>
             <h1 className="title-list-food">Menu</h1>
