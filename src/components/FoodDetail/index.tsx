b91d2ebbbcad71b296c8799f09b7c31f57f41b0a
sebas <sebastiansilvan97@gmail.com>
2023-07-24 12:11:19 +0200

    feat: cart on producto detalle added

--- src/components/FoodDetail/index.tsx
+++ src/components/FoodDetail/index.tsx
@@ -3,6 +3,7 @@ import { HiChevronLeft } from "react-icons/hi2";
 import { useHistory, useParams } from "react-router";
 import { UseFecthPost } from "../../api/post";
 import { ProductoDetalle } from "../../interfaces";
+import car_img from "./images/img_car.png";
 import "./foodetail.css";
 
 export const FoodDetail = () => {
@@ -37,27 +38,47 @@ export const FoodDetail = () => {
       </IonToolbar>
 
       {loading && <div>Cargando...</div>}
-      {food && (
-        <div className="main-container-food">
-          <h1>
-            <div className="food-image-container">
-              <img className="img-food-detail" src={food.media_url} />
-            </div>
-            <div className="food-description-container">
-              <div className="title-food">
-                {" "}
-                <h1>{food.nombreProducto}</h1>{" "}
-              </div>
-              <div className="categoria-food">
-                <h5>Categoria</h5>
-                <h5>{food.categoria}</h5>
-              </div>
-              <div className="description-food">
-                <p>{food.descripcion}</p>
-              </div>
-            </div>
-          </h1>
+      {error && (
+        <div>
+          {" "}
+          <h1>Error :</h1> {detaiError.toString()}
         </div>
+      )}
+      {food && (
+        <>
+          <div className="main-container-food">
+            <div className="container-food">
+              <h1>
+                <div className="food-image-container">
+                  <img className="img-food-detail" src={food.media_url} />
+                </div>
+                <div className="food-description-container">
+                  <div className="title-food">
+                    {" "}
+                    <h1>{food.nombreProducto}</h1>{" "}
+                  </div>
+                  <div className="categoria-food">
+                    <h5 style={{ color: "var(--ion-tab-bar-background)" }}>
+                      Categoría
+                    </h5>
+                    <h5>{food.categoria}</h5>
+                  </div>
+                  <div className="description-food">
+                    <p>{food.descripcion}</p>
+                  </div>
+                </div>
+              </h1>
+            </div>
+            <div className="car-options-container">
+              <button className="btn-container">
+                <img src={car_img} className="car-img" />
+                <div className="btn-info">Agregar al cariito</div>
+              </button>
+
+              <img className="car" src={car_img} />
+            </div>
+          </div>
+        </>
       )}
     </>
   );
