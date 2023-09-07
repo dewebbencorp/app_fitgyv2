4dbb592b8bdd85f97b7f8e5a4de5848e8807e513
sebas <sebastiansilvan97@gmail.com>
2023-09-07 16:59:40 -0500

    feat:  history screen desing implemented

--- src/components/PorducstPerPoints/History.tsx
+++ src/components/PorducstPerPoints/History.tsx
@@ -1,85 +1,85 @@
-import { useEffect, useState } from "react"
-import { Asociado, ComprasHistorial } from "../../interfaces"
+import { useEffect, useState } from "react";
+import { Asociado, ComprasHistorial } from "../../interfaces";
 import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { purchaseHistory } from "../../axios/Food";
+import { BiTransfer } from "react-icons/bi";
+import {
+  IonAccordion,
+  IonAccordionGroup,
+  IonCol,
+  IonGrid,
+  IonRow,
+} from "@ionic/react";
+import { Loading } from "../LoadScreen";
 
 interface PchHistoryProps {
-    user: number;
+  user: Asociado;
 }
-const tableStyles = {
-    borderCollapse: 'separate', // Cambiado a 'separate' para usar borderSpacing
-    borderSpacing: '0 10px',    // Espacio vertical de 10px entre filas
-    width: '100%',
-    border: '1px solid black'
-  };
-  
-  
-  const cellStyles = {
-    border: '1px solid white',
-    padding: '8px',
-    textAlign: 'left'
-  };
-  
-  const responsiveStyles = {
-    overflowX: 'auto',
-    backgroundColor: 'black',
-    margin:'1rem',
-    textAlign: 'center'
-  };
+
 export const PchHistory = ({ user }: PchHistoryProps) => {
-    const [history, setHis] = useState<ComprasHistorial[]>();
-    const dispatch: ThunkDispatch<any, void, AnyAction> = useDispatch();
+  const [history, setHis] = useState<ComprasHistorial[]>();
+  const dispatch: ThunkDispatch<any, void, AnyAction> = useDispatch();
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const getData = async () => {
-        try {
-            const his = await dispatch(purchaseHistory(user));
-            setHis(his);
-        } catch (error) {
-            console.error("Error fetching purchase history:", error);
-        }
-    };
+  const getData = async () => {
+    try {
+      const his = await dispatch(purchaseHistory(user.Clav_Asociado));
+      setHis(his);
+    } catch (error) {
+      console.error("Error fetching purchase history:", error);
+    }
+  };
 
-    return (
-        <>
-
-
-
-            {
-                history && history.map(list =>
-
-                    <div style={responsiveStyles}>
-                    <table style={tableStyles}>
-                      <tbody>
-                        <tr>
-                          <th style={cellStyles}>Detalle</th>
-                          <th style={cellStyles}>Ticket</th>
-                          <th style={cellStyles}>Monto</th>
-                          <th style={cellStyles}>Fecha</th>
-                          <th style={cellStyles}>Saldo</th>
-                          <th style={cellStyles}>Tipo</th>
-                          <th style={cellStyles}>Total</th>
-                        </tr>
-                        <tr>
-                          <td style={cellStyles}>{list.detalle}</td>
-                          <td style={cellStyles}>{list.ticket}</td>
-                          <td style={cellStyles}>{list.monto}</td>
-                          <td style={cellStyles}>{list.fecha}</td>
-                          <td style={cellStyles}>{list.saldo}</td>
-                          <td style={cellStyles}>{list.tipo}</td>
-                          <td style={cellStyles}>{list.total}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                 
-                  </div>
-
-                )
-            }
-        </>
-    );
+  return (
+    <>
+      <main>
+        <IonGrid>
+          <IonRow class="title-row">
+            <IonCol>Fecha</IonCol>
+            <IonCol>Abono/Cargo</IonCol>
+            <IonCol>Total</IonCol>
+          </IonRow>
+          {!history && <Loading />}
+          <section>
+            {history &&
+              history.map((list) => (
+                <>
+                  <IonAccordionGroup>
+                    <IonAccordion value="first">
+                      <div slot="header">
+                        <IonRow>
+                          <BiTransfer />
+                          <IonCol>{list.fecha}</IonCol>
+                          <IonCol>
+                            {list.tipo == "A " ? (
+                              <div style={{color:'greenyellow'}}>+{list.monto}</div>
+                            ) : (
+                              <div style={{color:'#ff7b5a'}}>-{list.monto}</div>
+                            )}
+                          </IonCol>
+                          <IonCol>{list.saldo} Pts</IonCol>
+                        </IonRow>
+                      </div>
+                      <div className="hdetail" slot="content">
+                        <h1>Producto</h1>
+                        <p>{list.detalle}</p>
+                      </div>
+                    </IonAccordion>
+                  </IonAccordionGroup>
+                </>
+              ))}
+          </section>
+        </IonGrid>
+      </main>
+    </>
+  );
 };
+
+/* ABONO = AUMENTA
+
+CARGO = DISMINUYE
+*/
--- src/components/PorducstPerPoints/index.tsx
+++ src/components/PorducstPerPoints/index.tsx
@@ -1,99 +1,107 @@
-import { useEffect, useState } from 'react'
-import './pp.css'
-import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
-import { useDispatch, useSelector } from 'react-redux';
-import { producsPerPoints } from '../../axios/Food';
-import { IonContent } from '@ionic/react';
-import { HiChevronLeft } from 'react-icons/hi2';
-import { Loading2 } from '../LoadScreen';
-import { Asociado, ProductosPorPuntos } from '../../interfaces';
-import { PchHistory } from './History';
-
+import { useEffect, useState } from "react";
+import "./pp.css";
+import { AnyAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { producsPerPoints } from "../../axios/Food";
+import { IonContent } from "@ionic/react";
+import { HiChevronLeft } from "react-icons/hi2";
+import { Loading2 } from "../LoadScreen";
+import { Asociado, ProductosPorPuntos } from "../../interfaces";
+import { PchHistory } from "./History";
+import logo from "./../../pages/Welcome/images/logo.png";
 
 export const PP = () => {
-    const user: Asociado = useSelector((state: Asociado) => state.user);
-    const dispatch: ThunkDispatch<any, void, AnyAction> = useDispatch();
-    const [dta, setDta] = useState<ProductosPorPuntos[]>()
-    const [isVisible, setIsVisible] = useState(false)
+  const user: Asociado = useSelector((state: Asociado) => state.user);
+  const dispatch: ThunkDispatch<any, void, AnyAction> = useDispatch();
+  const [dta, setDta] = useState<ProductosPorPuntos[]>();
+  const [isVisible, setIsVisible] = useState(false);
 
+  const backButtonHandler = () => {
+    window.location.href = "/home/perfil";
+  };
 
-
-    const backButtonHandler = () => {
-        window.location.href = '/home/perfil'
+  useEffect(() => {
+    getPp();
+    document.addEventListener("ionBackButton", backButtonHandler);
+    return () => {
+      document.removeEventListener("ionBackButton", backButtonHandler);
     };
+  }, []);
 
-
-    useEffect(() => {
-
-        getPp()
-        document.addEventListener('ionBackButton', backButtonHandler);
-        return () => {
-            document.removeEventListener('ionBackButton', backButtonHandler);
-        };
-    }, [])
-
-
-    const getPp = async () => {
-        try {
-            const data = await dispatch(producsPerPoints(user.puntos));
-            setDta(data)
-
-
-
-        } catch (error) {
-            console.error("Error fetching products:", error);
-        }
+  const getPp = async () => {
+    try {
+      const data = await dispatch(producsPerPoints(user.puntos));
+      setDta(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
     }
+  };
 
-
-
-    return (<>
-        <HiChevronLeft onClick={() => backButtonHandler()} style={{ fontSize: "3.2rem", marginBottom: "0rem" }} />
-
-        <IonContent>
-            <div className="pp-main-container">
-
-                <div className='pp-title'>
-                    <h1>{user.puntos} PTS</h1>
-                    <h3>¿Ya tienes planes para tus puntos?</h3>
-
-                    {isVisible ? <h3 className='isActive' onClick={() => setIsVisible(false)}>Lista de productos</h3> 
-                    : <h3 className='isActive' onClick={() => setIsVisible(true)}>VER HISTORIAL</h3>}
-
-                </div>
-
-
-                {
-                    isVisible ? <PchHistory user={user.Clav_Asociado} /> : <>
-                        {!dta && <> Cargando... <Loading2 /></>}
-
-                        {dta?.map(products =>
-
-                            <>
-                                <div className=" pp-card" key={products.clave}>
-                                    <div className="pts-container">
-                                        <div className="pts">
-                                            <h5 className="pts-font">{Math.floor(products.costo)} pts</h5>
-                                        </div>
-
-                                    </div>
-                                    <h3 className='pp-card-font' >{products.detalle}</h3>
-
-                                    {Math.floor(products.costo) > user.puntos ?
-                                        <h4 className='pp-card-font'>Ya casi es tuyo </h4> : <h4 className='pp-card-font'>Puedes tenerlo</h4>}
-
-                                </div>
-
-                            </>
-                        )}</>
-                }
-
-
-
+  return (
+    <>
+      <IonContent>
+        {isVisible ? (
+          <>
+            <div className="isActive" onClick={() => setIsVisible(false)}>
+              <img src={logo} />
+             <div> <HiChevronLeft/></div>
+             
             </div>
-        </IonContent>
+            <PchHistory user={user} />
+          </>
+        ) : (
+          <>
+            <HiChevronLeft
+              onClick={() => backButtonHandler()}
+              style={{ fontSize: "3.2rem", marginBottom: "0rem" }}
+            />
+            <div className="pp-main-container">
+              <div className="pp-title">
+                <h1>{user.puntos} PTS</h1>
+                <h3>¿Ya tienes planes para tus puntos?</h3>
 
+                {!isVisible && (
+                  <h3 style={{marginTop: '2vh', color: 'orangered' }} onClick={() => setIsVisible(true)}>
+                    VER HISTORIAL
+                  </h3>
+                )}
+              </div>
 
+              {!isVisible && (
+                <>
+                  {!dta && (
+                    <>
+                      {" "}
+                      Cargando... <Loading2 />
+                    </>
+                  )}
 
-    </>)
-}
+                  {dta?.map((products) => (
+                    <>
+                      <div className=" pp-card" key={products.clave}>
+                        <div className="pts-container">
+                          <div className="pts">
+                            <h5 className="pts-font">
+                              {Math.floor(products.costo)} pts
+                            </h5>
+                          </div>
+                        </div>
+                        <h3 className="pp-card-font">{products.detalle}</h3>
+
+                        {Math.floor(products.costo) > user.puntos ? (
+                          <h4 className="pp-card-font">Ya casi es tuyo </h4>
+                        ) : (
+                          <h4 className="pp-card-font">Puedes tenerlo</h4>
+                        )}
+                      </div>
+                    </>
+                  ))}
+                </>
+              )}
+            </div>
+          </>
+        )}
+      </IonContent>
+    </>
+  );
+};
