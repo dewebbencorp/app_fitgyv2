0c039569a576d97e33f5c8229187bba240f861a8
sebas <sebastiansilvan97@gmail.com>
2023-10-12 16:27:53 -0500

    update: news fetch

--- src/components/News/fullScreenImage.tsx
+++ src/components/News/fullScreenImage.tsx
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { IonModal } from "@ionic/react";
 import "./news.css";
 
 export const FullScreenImage = ({ Image, setModal }: any) => {
+  const backButtonHandler = () => {
+    setModal(false); // Cerrar el modal
+  };
 
-    const backButtonHandler = () => {
-        setModal(false); // Cerrar el modal
+  useEffect(() => {
+    document.addEventListener("ionBackButton", backButtonHandler);
+    return () => {
+      document.removeEventListener("ionBackButton", backButtonHandler);
     };
+  }, []);
 
-    useEffect(() => {
-        document.addEventListener('ionBackButton', backButtonHandler);
-        return () => {
-            document.removeEventListener('ionBackButton', backButtonHandler);
-        };
-    }, []);
+  return (
+    <IonModal className="i-modal" isOpen={true}>
+      <div className="full-image-container">
+        <div
+          style={{
+            position:'absolute',
+            fontSize: "2rem",
+            display: "flex",
+            justifyContent: "end",
+            top:'1rem',
+            width: "100%",
+            marginRight: "0.7rem",
+          }}
+        >
+          <AiOutlineCloseCircle onClick={() => backButtonHandler()} />
+        </div>
 
-    return (
-        <IonModal className="i-modal" isOpen={true}>
-            <div className="full-image-container">
-
-                <AiOutlineCloseCircle className="btn-close-new" onClick={() => backButtonHandler()} />
-                <img src={Image} />
-
-            </div>
-        </IonModal>
-    );
+        <img src={Image} />
+      </div>
+    </IonModal>
+  );
 };
