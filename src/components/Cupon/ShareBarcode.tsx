3cbfcc4886a436035e79f4089006f45dadc6f1d8
Sebastian Matinez <desarrollofullstack@bencorp.com.mx>
2024-03-20 16:38:07 -0500

    update: detail ticket desing

--- src/components/Cupon/ShareBarcode.tsx
+++ src/components/Cupon/ShareBarcode.tsx
@@ -5,6 +5,8 @@ import Barcode from "react-barcode";
 import "./cupon.css";
 import logo from "./images/fitbar.png";
 import toast from "react-hot-toast";
+import { Asociado } from "../../interfaces";
+import { useSelector } from "react-redux";
 
 interface ShareBarcodeProps {
   code: string;
@@ -13,9 +15,11 @@ interface ShareBarcodeProps {
 
 export const ShareBarcode = ({ code, name }: ShareBarcodeProps) => {
   const barcodeRef = useRef<HTMLDivElement | null>(null);
+  const user: Asociado = useSelector((state: Asociado) => state.user);
   const [date, setDate] = useState<Date | null>(null);
   const [isActive, setIsActive] = useState(false);
   const [isShared, setIsShared] = useState(false);
+
   useEffect(() => {
     const today = new Date();
     setDate(today);
@@ -51,27 +55,52 @@ export const ShareBarcode = ({ code, name }: ShareBarcodeProps) => {
 
   return (
     <main className=" flex flex-col  justify-center items-center gap-10  h-screen ">
-      {/* <h1 className="  text-center  items-center poppins text-[1.5rem] bold tracking-[0.2rem] italic ">
+      <h1 className=" absolute z-30 top-[5vh]  text-center  items-center poppins italic bold text-[1.5rem]  tracking-[0.2rem]  ">
         ¡CUPÓN EXITOSO!
-      </h1> */}
+      </h1>
 
-      
-
-      <section className="flex justify-center items-center z-10 border-[2px] border-[#e64e08]  h-[70%] w-[85%] p-10 rounded-[2rem]  relative bg-[var(--ion-background-color)]  ">
-      <img
+      <section className="flex justify-center items-center z-10 border-[2px] border-[#e64e08]  h-[65%] w-[85%]  rounded-[2rem]  relative bg-[var(--ion-background-color)]  ">
+        <img
           src={logo}
-          className="absolute w-40 top-[-5rem] bg-gradient-to-r  from-[#e64e08] to-[#ff7d04] p-3  rounded-full border-[var(--ion-background-color)] border-[0.7rem] "
+          className="absolute w-36 top-[-5rem] bg-gradient-to-r  from-[#e64e08] to-[#ff7d04] p-3  rounded-full border-[var(--ion-background-color)] border-[0.7rem] "
         />
         <div className=" square-left"></div>
         <div className=" square-right"></div>
 
-        {/* <div className="" ref={barcodeRef}>
-          <div>
-            <Barcode value={code} />
-          </div>
-        </div> */}
+        <section className=" flex flex-col  justify-start    mt-10  h-[75%] w-[100%] ">
+          <h1 className="text-center text-[1.5rem] poppins bold tracking-[0.2rem] italic mb-5 ">
+            CUPÓN
+          </h1>
 
-        <button
+          <section className="flex ml-[0.5rem] mr-[0.5rem] p-1 h-[20%]  items-start   child:text-white child:text-[0.9rem] text-center poppins ">
+            <span>Generó: {user.Nombre_Asociado + " " + user.Apellidos}</span>
+            <span className="bg-white w-[0.7%] h-[100%] text-white "></span>
+            <span>Beneficiario : SEBASTIAN MARTÍNEZ</span>
+          </section>
+
+          <section className="flex flex-col items-center gap-1 mt-3  ml-[0.5rem] mr-[0.5rem] p-1      child:text-white child:text-[0.9rem] text-start poppins ">
+            <span>Fecha: 20/01/24</span>
+            <span>Hora: 4:46:06</span>
+            <span>Vencimiento: 2/02/24</span>
+          </section>
+
+          <section className="flex flex-col justify-center items-center  child:text-white  mt-2 ">
+            <span className="text-[0.9rem] poppins">Monto:</span>
+            <span className="text-[2rem] poppins">$200</span>
+          </section>
+
+          <section className="  flex justify-center h-[20] " ref={barcodeRef}>
+            
+              <Barcode  height={15} width={1}  value={code} />
+            
+          </section>
+
+
+          <section className="flex justify-center mt-5 ">
+            hola
+          </section>
+        </section>
+        {/* <button
           className="absolute bottom-5 bg-[orangered] p-1 rounded-md"
           onClick={handleGenerateAndShare}
           disabled={isActive}
@@ -87,7 +116,7 @@ export const ShareBarcode = ({ code, name }: ShareBarcodeProps) => {
               </p>
             )}
           </div>
-        </button>
+        </button> */}
       </section>
     </main>
   );
